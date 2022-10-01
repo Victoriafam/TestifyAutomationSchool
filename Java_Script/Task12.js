bbfaa18cb360902c245409e2eca20438de227724
Victoria Adejoke Famuwagun <famuwagunadejoke@gmail.com>
2022-10-01 23:28:47 +0100

    update Java_Script

---
+++ Java_Script/Task12.js
@@ -0,0 +1,19 @@
+// My Personal Library 
+
+const books = {
+title: 'The Subtle Art of Not Giving a F*ck A Counterintuitive Approach to Living a Good Life',
+description: 'Unconventional Pragmatic advice',
+numberOfPages:100,
+author: 'Mark Manson',
+reading: true,
+toggleReadingStatus: function () {
+  if (books.reading === true) {
+    books.reading = false 
+  } else {
+    books.reading = true 
+  }
+}
+}
+books.reading = false;
+books.toggleReadingStatus();
+console.log(books.reading)
