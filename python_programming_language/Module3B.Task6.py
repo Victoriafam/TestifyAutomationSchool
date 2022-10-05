bd3d5d7cce8d523e597d5f8024b2a167a19943c8
Victoria Adejoke Famuwagun <famuwagunadejoke@gmail.com>
2022-10-05 13:26:43 +0100

    Conditional Statements and logical Operations

---
+++ python_programming_language/Module3B.Task6.py
@@ -0,0 +1,8 @@
+# TASK 6
+# 1. Declare two numbers with any values
+# 2. Check if the first number is greater than 50 and check if the second number is less than 100,
+#    if True print out "Evaluation is True"
+# 3. Add an else block that prints "Evaluation is False"
+# 4. Declare a string with value "python"
+# 5. Check if the first string is equal to "testify" or "python", if True prints out "Yay!!
+#    The string is a Testify Python".
