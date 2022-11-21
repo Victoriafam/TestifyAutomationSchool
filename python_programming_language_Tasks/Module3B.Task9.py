10c888a98e21a1e0c9f896ae954681d3591089af
Victoria Adejoke Famuwagun <famuwagunadejoke@gmail.com>
2022-11-22 00:20:19 +0100

    Functions

---
+++ python_programming_language_Tasks/Module3B.Task9.py
@@ -0,0 +1,14 @@
+
+#Lesson 9 Task9
+#1.	Create a function that prints out "Hello World"
+#2.	Create a function that does nothing, an empty function
+#3.	Invoke/call both functions
+
+print("functions\n")
+def greetings():
+print("Hello World")
+def nothing():
+print()
+greetings()
+nothing()
+
