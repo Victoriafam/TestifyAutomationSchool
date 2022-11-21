bc9d2dbbc77c36144db3527ced1cd2d45a27259e
Victoria Adejoke Famuwagun <famuwagunadejoke@gmail.com>
2022-11-22 00:51:01 +0100

    Arguments And Return Statement

---
+++ python_programming_language_Tasks/Module3B.Task11.py
@@ -0,0 +1,21 @@
+#Lesson 11 Task
+#1.	Create a function that accepts two numbers, adds the numbers and prints out the result in the console.
+#2.	Create a function that return the string value "Testify Python"
+#3.	Invoke/call the two functions
+
+print("\ndefault parameter")
+def add(num1=10, num2=15):
+ result = num1 + num2
+ print("result", result)
+
+add()
+add(10)
+add(10, 15)
+
+print("Testify Python")
+# required parameter
+def greetings(name):
+ print ("Hello",Welcome)
+ greetings()
+
+
