448b4051dc2774416fac011a99919ab9da3c56d7
Victoria Adejoke Famuwagun <famuwagunadejoke@gmail.com>
2022-11-22 01:02:16 +0100

    Introduction to Object-Oriented Programming and  Attributes

---
+++ python_programming_language_Tasks/Module3B.Task18.py
@@ -0,0 +1,19 @@
+# Lesson 18 Task=
+# Create a class called Human
+# Add an attribute leg_count with the value of 4
+#  Add another attribute can_walk with value of True
+# Optionally you can instantiate the class and prints out the leg_count and can_walk attributes
+
+# class
+class Human:
+    group = "Black"
+    leg_count = 4
+    can_walk = True
+ def __init__(self, name):
+     self.name = name
+     # Object
+
+Human = Human("Negroid")
+Human = Human ("Mongoloid")
+print(Negroid.name)
+print (Mongoloid.name)
