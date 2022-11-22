c7fa21633ab027a5e009a9bd22932c4e5ed4bb11
Victoria Adejoke Famuwagun <famuwagunadejoke@gmail.com>
2022-11-22 01:03:26 +0100

    OOP Methods

---
+++ python_programming_language_Tasks/Module3B.Task19.py
@@ -0,0 +1,24 @@
+
+
+# Lesson 19 Task
+# 1.	Create a class called Human
+# 2.	Add an attribute leg_count with the value of 4
+# 3.	Add another attribute can_walk with the value of True
+# 4.	Create a method called get_description, the method should print "This is human"
+# 5.	Create another method that return the leg count, the name of the method is your choice
+#  Optionally you can instantiate the class and invoke the method get_description()
+#  and invoke your method that returns leg count
+
+class Human:
+
+    leg_count = 4
+    can_walk = True
+
+    def get_description(self, description):
+         return description
+    def get_legCount(self):
+        return self.leg_count
+Human = Human()
+print(Human.get_description("This is human"))
+ Man = Human()
+print(Man.leg_count)
