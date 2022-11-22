544b344a20def778d7ada4ec668ae712d5d62e4c
Victoria Adejoke Famuwagun <famuwagunadejoke@gmail.com>
2022-11-22 01:04:19 +0100

    Principles of OOP - Inheritance

---
+++ python_programming_language_Tasks/Module3B.Task20.py
@@ -0,0 +1,18 @@
+# Lesson 20 Task#
+# 1.	Create a class called Human
+# 2.	Add an attribute leg_count with the value of 4
+# 3.	Add a method called get_gender() that returns "Unknown" in the Human class
+# 4.	Create another class called Man that extends HumanOptionally you can instantiate the classes Man and Woman then print out the values of the get_gender() method in each object instance
+
+class Human:
+    gender = "Unknown"
+    leg_count = 4
+    def get_gender(self):
+    self.name = gender
+
+ class Man:
+
+    self.name = "Unknown"
+
+class Male:
+    dfe
