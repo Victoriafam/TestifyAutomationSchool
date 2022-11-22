98c6ad60851dc6d3fe47ed839ef465c5ef5f206c
Victoria Adejoke Famuwagun <famuwagunadejoke@gmail.com>
2022-11-22 01:00:56 +0100

    Basic List Operations

---
+++ python_programming_language_Tasks/Module3B.Task15.py
@@ -0,0 +1,31 @@
+#Lesson 15 Task
+#1.	Declare a list with any value and number of item/element
+#2.	Print the list in console
+#3.	Sort the list
+#4.	Print the list in console again
+#Note: you can experiment with the other list functions too in the task.
+
+# List operators
+Food = ["Fried Rice", "Dodo", "Fufu", "Amala", "Fish", "Ponmo", "Egusi", "Dodo", "Dodo"]
+print("List:", Food)
+
+# sort -> sort the items in the list by either ascending or descending order
+Food.sort()
+print("sort-asc:", Food)
+Food.sort(reverse=True)
+print("sort-desc:", Food)
+
+# List operators
+Countries = ["Afghanistan", "Algeria", "Denmark", "Libya", "Nigeria ", "Mali", "Netherlands", "South Africa", "Switzerland", "Turkey",  "United Arab Emirates"]
+print("List:", Countries)
+
+# sort -> sort the items in the list by either ascending or descending order
+Countries.sort()
+print("sort-asc:", Countries)
+Countries.sort(reverse=True)
+print("sort-desc:", Countries)
+
+
+
+
+
