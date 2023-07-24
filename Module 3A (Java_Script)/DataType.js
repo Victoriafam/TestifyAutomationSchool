0c78dc6b6eb0e41050d8462e21afe8eb5fca4e78
Victoria Adejoke Famuwagun <famuwagunadejoke@gmail.com>
2023-07-24 15:54:20 +0100

    Datatype

---
+++ Module 3A (Java_Script)/DataType.js
@@ -0,0 +1,27 @@
+// // const myAge = "Thrity Four"
+// // typeof(myAge)
+
+// // console.log(typeof(myAge))
+
+// // const myAge = 34
+
+// // console.log(myAge * 2)
+
+
+// const myFavouriteFood = 'Amala'
+
+// const age = 34
+// const name = 'Victoria'
+
+// const sentence = 'My name is '  +
+
+
+// console.log('My favourite food is '+myFavouriteFood)
+
+const isBritish = true
+const isNight = false
+const userDetails = null
+
+console.log(typeof(isNight))
+
+console.log(typeof(userDetails))
