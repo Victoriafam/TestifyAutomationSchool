e62792e06448e8850cecc6a1974666252b0f7627
Victoria Adejoke Famuwagun <famuwagunadejoke@gmail.com>
2023-07-24 15:52:24 +0100

    Comparision

---
+++ Module 3A (Java_Script)/Comparision.js
@@ -0,0 +1,37 @@
+// // // const compare = 8 == "8"
+
+// // // console.log(compare)
+
+// // const userName = "Victoriafam"
+// // const age = 34
+// // const logic = age === 34 && userName === "Victoria"
+
+// // console.log(logic)
+
+
+// const userName = "Victoriafam"
+// const age = 34
+// const logic = age === 34 || userName === "Victoriafam"
+
+// console.log(logic)
+
+
+// // const age = 15
+
+// // if (age >= 18) {
+// //     console.log('You are elegible to vote')
+// // } else{
+// //     console.log('You are not elegible to vote')
+
+// }
+
+const age = 78
+if (age>=18 && age is <=65) {
+
+    console.log('You are elegible to vote')
+} else if (age>65) {
+    console.log('You are too old to vote')
+    } else {
+
+console.log('You are too young to vote')
+}
